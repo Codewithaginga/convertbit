db2b99b78620054badace2b081224154bc8c3899
aginga <brianaginga35@gmail.com>
2022-09-30 09:40:05 +0300

    new program

--- bcoin.py
+++ bcoin.py
@@ -1,1 +1,34 @@
-# bitcoin programm
+# bitcoin program
+
+import time
+
+
+class Bitcoin:
+
+    ti = time.asctime()
+
+    bitcoin1 = '$2086'
+
+    print(f'As {ti} bitcoin is currently trading at {bitcoin1} usd')
+
+    @staticmethod
+    def enter_coin():
+
+        bit = 2086
+
+        enter_bitcoin_amount = float(input('\33[1mEnter amount of bitcoin amount: ' '\n'))
+
+        usd = bit * enter_bitcoin_amount
+
+        print(f'That is worth {usd:,} usd')
+
+
+b = Bitcoin()
+b.enter_coin()
+
+
+
+
+
+
+
